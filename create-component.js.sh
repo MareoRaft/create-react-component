bbd64c3950ab4e6e63d2b7fa952fbda583192a97
Matthew Lancellotti <mareoraft@gmail.com>
2019-04-01 14:44:45 -0400

    Update create-component.js.sh
    module.scss

--- create-component.js.sh
+++ create-component.js.sh
@@ -1,7 +1,7 @@
 echo "import React from 'react';
 import PropTypes from 'prop-types';
 
-import styles from './$1.scss';
+import styles from './$1.module.scss';
 
 class $1 extends React.Component {
 	render() {
